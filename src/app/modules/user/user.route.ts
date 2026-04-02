570c8af3015958e56c916bd2107c8360445ba18f
nahidraz23 <nzamanraz@gmail.com>
2026-04-02 17:30:15 +0600

    feat: get-user route authorization added

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,10 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserControllers } from "./user.controller";
 import { createUserZodSchema } from "./user.validation";
 import { validateRequest } from "../../middlewares/validateRequest";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import AppError from "../../errorHelpers/AppError";
+import { Role } from "./user.interface";
 
 const router = Router();
 
@@ -10,6 +13,28 @@ router.post(
     validateRequest(createUserZodSchema),
     UserControllers.createUser,
 );
-router.get("/all-users", UserControllers.getAllUsers);
+router.get(
+    "/all-users",
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const accessToken = req.headers.authorization;
+
+            if (!accessToken) {
+                throw new AppError(403, "No token received");
+            }
+
+            const verifiedToken = jwt.verify(accessToken, "secret");
+
+            if((verifiedToken as JwtPayload).role !== Role.ADMIN || Role.SUPER_ADMIN) {
+                throw new AppError(403, "You are not permitted to view this route");
+            }
+
+            next()
+        } catch (error) {
+            next(error)
+        }
+    },
+    UserControllers.getAllUsers,
+);
 
 export const UserRoutes = router;
