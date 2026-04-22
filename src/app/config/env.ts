8b579aba19b2b0ef854fbc5f7e8b496f9084c6c6
nahidraz23 <nzamanraz@gmail.com>
2026-04-22 06:23:49 +0600

    feat(auth): refresh token generate

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -3,35 +3,50 @@ import dotenv from "dotenv";
 dotenv.config();
 
 interface IEnvConfig {
-  PORT: string;
-  DB_URL: string;
-  NODE_ENV: 'development' | 'production',
-  BCRYPT_SALT_ROUND: string,
-  JWT_ACCESS_SECRET: string,
-  JWT_ACCESS_EXPIRES: string,
-  SUPER_ADMIN_EMAIL: string,
-  SUPER_ADMIN_PASSWORD: string
+    PORT: string;
+    DB_URL: string;
+    NODE_ENV: "development" | "production";
+    BCRYPT_SALT_ROUND: string;
+    JWT_ACCESS_SECRET: string;
+    JWT_ACCESS_EXPIRES: string;
+    JWT_REFRESH_SECRET: string;
+    JWT_REFRESH_EXPIRES: string;
+    SUPER_ADMIN_EMAIL: string;
+    SUPER_ADMIN_PASSWORD: string;
 }
 
 const loadEnvVariable = (): IEnvConfig => {
-    const requiredEnvVariables : string [] = ['PORT', 'DB_URL', 'NODE_ENV', "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_EXPIRES", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD"];
+    const requiredEnvVariables: string[] = [
+        "PORT",
+        "DB_URL",
+        "NODE_ENV",
+        "BCRYPT_SALT_ROUND",
+        "JWT_ACCESS_EXPIRES",
+        "JWT_ACCESS_EXPIRES",
+        "SUPER_ADMIN_EMAIL",
+        "SUPER_ADMIN_PASSWORD",
+        "JWT_REFRESH_SECRET",
+        "JWT_REFRESH_EXPIRES"
+    ];
 
-    requiredEnvVariables.forEach(key => {
-        if(!process.env[key]){
-            throw new Error(`Missing required environment variable ${key}`)
+    requiredEnvVariables.forEach((key) => {
+        if (!process.env[key]) {
+            throw new Error(`Missing required environment variable ${key}`);
         }
-    })
+    });
 
-  return {
-    PORT: process.env.PORT as string,
-    DB_URL: process.env.DB_URL as string,
-    NODE_ENV: process.env.NODE_ENV as 'development' | 'production',
-    BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
-    JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
-    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
-    SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
-    SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
-  };
+    return {
+        PORT: process.env.PORT as string,
+        DB_URL: process.env.DB_URL as string,
+        NODE_ENV: process.env.NODE_ENV as "development" | "production",
+        BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+        JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+        JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+        JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+        JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
+        SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
+        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
+    };
 };
 
 export const envVars: IEnvConfig = loadEnvVariable();
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -30,10 +30,25 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
         role: isUserExist.role,
     };
 
-    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES);
+    const accessToken = generateToken(
+        jwtPayload,
+        envVars.JWT_ACCESS_SECRET,
+        envVars.JWT_ACCESS_EXPIRES,
+    );
+
+    const refreshToken = generateToken(
+        jwtPayload,
+        envVars.JWT_REFRESH_SECRET,
+        envVars.JWT_REFRESH_EXPIRES,
+    );
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: pass, ...rest } = isUserExist.toObject();
 
     return {
         accessToken,
+        refreshToken,
+        user: rest,
     };
 };
 
